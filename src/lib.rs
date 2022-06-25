49a2fd890feef591b31c4585654f831a3b3192e5
yeosiowvic <victoryeo1974@gmail.com>
2022-06-25 14:52:56 +0800

    implement Worker struct for ThreadPool

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,37 @@
 use std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
     pub fn new(_size: usize) -> ThreadPool {
         assert!(_size > 0);
-        let mut threads = Vec::with_capacity(_size);
+        let mut workers = Vec::with_capacity(_size);
 
-        for _ in 0.._size {
-            // create some threads and store them in the vector
+        for id in 0.._size {
+            workers.push(Worker::new(id));
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
     }
 
     pub fn execute<F>(&self, _f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
