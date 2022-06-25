3f58ac74f85348ca9924613584461715ca7b0cdb
yeosiowvic <victoryeo1974@gmail.com>
2022-06-25 14:50:11 +0800

    init ThreadPool

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool {
     pub fn new(_size: usize) -> ThreadPool {
         assert!(_size > 0);
-        ThreadPool
+        let mut threads = Vec::with_capacity(_size);
+
+        for _ in 0.._size {
+            // create some threads and store them in the vector
+        }
+
+        ThreadPool { threads }
     }
 
     pub fn execute<F>(&self, _f: F)
